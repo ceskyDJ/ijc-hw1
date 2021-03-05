6266a781ea9fcb2f3cb5009f2a0eb488fdc673d9
ceskyDJ <48548230+ceskyDJ@users.noreply.github.com>
2021-03-05 20:47:32 +0100

    Added the first version of error.c

--- error.c
+++ error.c
@@ -5,3 +5,35 @@
 
 #include "error.h"
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Writes warning message to the standard error output
+ * @param fmt Formatted message string (like the first parameter of printf())
+ * @param ... Values for replacing tags in the first parameter
+ */
+void warning_msg(const char *fmt, ...) {
+    va_list tag_replacements;
+
+    va_start(tag_replacements, fmt);
+    fprintf(stderr, "CHYBA: ");
+    vfprintf(stderr, fmt, tag_replacements);
+    va_end(tag_replacements);
+}
+
+/**
+ * Writes error message to the standard error output and stops program with error code
+ * @param fmt Formatted message string (like the first parameter of printf())
+ * @param ... Values for replacing tags in the first parameter
+ */
+void error_exit(const char *fmt, ...) {
+    va_list tag_replacements;
+
+    va_start(tag_replacements, fmt);
+    fprintf(stderr, "CHYBA: ");
+    vfprintf(stderr, fmt, tag_replacements);
+    va_end(tag_replacements);
+
+    exit(EXIT_FAILURE);
+}
--- error.h
+++ error.h
@@ -6,7 +6,17 @@
 #ifndef IJC_1_HOMEWORK_ERROR_H
 #define IJC_1_HOMEWORK_ERROR_H
 
+/**
+ * Writes warning message to the standard error output
+ * @param fmt Formatted message string (like the first parameter of printf())
+ * @param ... Values for replacing tags in the first parameter
+ */
 void warning_msg(const char *fmt, ...);
+/**
+ * Writes error message to the standard error output and stops program with error code
+ * @param fmt Formatted message string (like the first parameter of printf())
+ * @param ... Values for replacing tags in the first parameter
+ */
 void error_exit(const char *fmt, ...);
 
 #endif //IJC_1_HOMEWORK_ERROR_H
