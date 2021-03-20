f89ac721f2296036923515c4088ad0dfef017732
ceskyDJ <48548230+ceskyDJ@users.noreply.github.com>
2021-03-20 18:50:42 +0100

    Removed round() from eratosthenes.c

--- eratosthenes.c
+++ eratosthenes.c
@@ -10,7 +10,7 @@ void eratosthenes(bitset_t bit_array) {
     bitset_setbit(bit_array, 0, 1);
     bitset_setbit(bit_array, 1, 1);
 
-    for (unsigned long i = 2; i <= (unsigned long)round(sqrt((double)bitset_size(bit_array))); i++) {
+    for (unsigned long i = 2; i <= (unsigned long)sqrt((double)bitset_size(bit_array)); i++) {
         if (bitset_getbit(bit_array, i) == 0) {
             for (unsigned long n = 2; (n * i) < bitset_size(bit_array); n++) {
                 bitset_setbit(bit_array, n * i, 1);
